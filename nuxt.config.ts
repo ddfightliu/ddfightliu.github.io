425790e79d6ccce67f39b604038e598daa12dc26
liuddfight <ddfightliu@hotmail.com>
2025-05-06 09:43:42 +0000

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+  app: {
+    baseURL: '/ddfightliu.github.io/'
+  },
   devtools: { enabled: true },
   alias: {
     // 确保别名指向正确的目录
